78080b5d25001c9fe33b07240580de1b6298e056
Pavel Surkov <surkovv.pavel@gmail.com>
2022-10-22 18:34:15 +0700

    adds gulp functions for phpmailer

--- gulpfile.js
+++ gulpfile.js
@@ -10,42 +10,65 @@ const del = require('del');
 
 sass.compiler = require('node-sass');
 
-gulp.task('styles', function(){
-    return gulp.src('app/scss/main.scss')
+gulp.task('styles', function () {
+  return gulp
+    .src('app/scss/main.scss')
     .pipe(sass())
     .pipe(concat('application.css'))
     .pipe(postcss([autoprefixer()]))
-    .pipe(gulp.dest('public/css/'))
+    .pipe(gulp.dest('public/css/'));
 });
 
-gulp.task('scripts', function(){
-    return gulp.src(['app/js/plugins.js', 'app/js/main.js'])
+gulp.task('scripts', function () {
+  return gulp
+    .src(['app/js/plugins.js', 'app/js/main.js'])
     .pipe(concat('application.js'))
-    .pipe(gulp.dest('public/js/'))
+    .pipe(gulp.dest('public/js/'));
 });
 
-gulp.task('clean', function(){
-    return del('public');
+gulp.task('clean', function () {
+  return del('public');
 });
 
-gulp.task('assets', function(){
-    return gulp.src('app/assets/**', {since: gulp.lastRun('assets')})
-        .pipe(gulp.dest('public'))
+gulp.task('assets', function () {
+  return gulp
+    .src('app/assets/**', { since: gulp.lastRun('assets') })
+    .pipe(gulp.dest('public'));
 });
 
-gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'scripts', 'assets')));
-
-gulp.task('watch', function(){
-    gulp.watch('app/scss/**/*.*', gulp.series('styles'));
-    gulp.watch('app/js/**/*.*', gulp.series('scripts'));
-    gulp.watch('app/assets/**/*.*', gulp.series('assets'));
+gulp.task('php', function () {
+  return gulp
+    .src('send-form.php', { since: gulp.lastRun('php') })
+    .pipe(gulp.dest('public'));
 });
 
-gulp.task('serve', function(){
-    browserSync.init({
-        server: 'public'
-    });
-    browserSync.watch('public/**/*.*').on('change', browserSync.reload);
+gulp.task('phpmailer', function () {
+  return gulp
+    .src('app/phpmailer/**', { since: gulp.lastRun('phpmailer') })
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task(
+  'build',
+  gulp.series(
+    'clean',
+    gulp.parallel('styles', 'scripts', 'assets', 'php', 'phpmailer')
+  )
+);
+
+gulp.task('watch', function () {
+  gulp.watch('app/scss/**/*.*', gulp.series('styles'));
+  gulp.watch('app/js/**/*.*', gulp.series('scripts'));
+  gulp.watch('app/assets/**/*.*', gulp.series('assets'));
+  gulp.watch('send-form.php', gulp.series('php'));
+  gulp.watch('app/phpmailer/*.*', gulp.series('phpmailer'));
+});
+
+gulp.task('serve', function () {
+  browserSync.init({
+    server: 'public',
+  });
+  browserSync.watch('public/**/*.*').on('change', browserSync.reload);
 });
 
 gulp.task('dev', gulp.series('build', gulp.parallel('watch', 'serve')));
