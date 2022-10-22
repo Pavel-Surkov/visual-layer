02e9a159375c4f264aa89839cde945171b0207b6
Pavel Surkov <surkovv.pavel@gmail.com>
2022-10-22 22:05:44 +0700

    adds popup

--- public/js/application.js
+++ public/js/application.js
@@ -4,3 +4,27 @@
 document.addEventListener(`DOMContentLoaded`, () => {});
 
 // JQUERY
+
+// Form popup
+const signupForm = document.querySelector('#signup-form');
+const closeBtn = document.querySelector('#signup-close');
+const signupModal = document.querySelector('#signup-modal');
+const blocker = document.querySelector('#blocker');
+const greatBtn = document.querySelector('#signup-modal .btn-black');
+
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  signupModal.classList.add('signup-popup_opened');
+  blocker.classList.add('blocker_opened');
+});
+
+greatBtn.addEventListener('click', () => {
+  signupModal.classList.remove('signup-popup_opened');
+  blocker.classList.remove('blocker_opened');
+});
+
+closeBtn.addEventListener('click', () => {
+  signupModal.classList.remove('signup-popup_opened');
+  blocker.classList.remove('blocker_opened');
+});
